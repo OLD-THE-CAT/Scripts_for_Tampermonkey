6220ecd0b06d05ab0a2e23bb590780cf269f0ca4
STAS <chappiechempion@gmail.com>
2026-03-07 20:07:59 +0500

    Update GAds. Показываем остаток бюджета-1.3.user.js

--- GAds. Показываем остаток бюджета-1.3.user.js
+++ GAds. Показываем остаток бюджета-1.3.user.js
@@ -6,8 +6,8 @@
 // @author       ИП Ульянов (Станислав)
 // @match        https://ads.google.com/*
 // @include      https://ads.google.com/*
-// @updateURL
-// @downloadURL
+// @updateURL    https://github.com/OLD-THE-CAT/Scripts_for_Tampermonkey/raw/refs/heads/main/GAds.%20%D0%9F%D0%BE%D0%BA%D0%B0%D0%B7%D1%8B%D0%B2%D0%B0%D0%B5%D0%BC%20%D0%BE%D1%81%D1%82%D0%B0%D1%82%D0%BE%D0%BA%20%D0%B1%D1%8E%D0%B4%D0%B6%D0%B5%D1%82%D0%B0-1.3.user.js
+// @downloadURL  https://github.com/OLD-THE-CAT/Scripts_for_Tampermonkey/raw/refs/heads/main/GAds.%20%D0%9F%D0%BE%D0%BA%D0%B0%D0%B7%D1%8B%D0%B2%D0%B0%D0%B5%D0%BC%20%D0%BE%D1%81%D1%82%D0%B0%D1%82%D0%BE%D0%BA%20%D0%B1%D1%8E%D0%B4%D0%B6%D0%B5%D1%82%D0%B0-1.3.user.js
 // @icon         https://img.icons8.com/color/48/google-ads.png
 // @grant        none
 // ==/UserScript==
